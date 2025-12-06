1b3e97611b4cb1430246e5fc0e447e5ed3e21ba6
Marek Toome <marekumail@gmail.com>
2025-12-06 21:44:28 +0200

    Add SaveCustomerCommand

---
+++ KooliProjekt.Application/Features/Customers/SaveCustomerCommand.cs
@@ -0,0 +1,22 @@
+﻿using KooliProjekt.Application.Behaviors;
+using KooliProjekt.Application.Infrastructure.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KooliProjekt.Application.Features.Customers
+{
+    public class SaveCustomerCommand : IRequest<OperationResult>, ITransactional
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
