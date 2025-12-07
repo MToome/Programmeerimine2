b345b82fb4476cf8deb8d44ad75f9371b2d593bc
Marek Toome <marekumail@gmail.com>
2025-12-07 08:43:05 +0200

    Add SaveInvoiceCommand

---
+++ KooliProjekt.Application/Features/Invoices/SaveInvoiceCommand.cs
@@ -0,0 +1,22 @@
+﻿using KooliProjekt.Application.Behaviors;
+using KooliProjekt.Application.Infrastructure.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KooliProjekt.Application.Features.Customers
+{
+    public class SaveInvoiceCommand : IRequest<OperationResult>, ITransactional
+    {
+        public int Id { get; set; }
+        
+        public DateTime Date { get; set; }
+        public DateTime DueDate { get; set; }
+        public int CustomerId { get; set; }
+        public string Customer { get; set; }
+        public List<int> Items { get; set; }
+    }
+}
