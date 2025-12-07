b34b9798b8a5f1b938d1e0fb87b452d2107e1262
Marek Toome <marekumail@gmail.com>
2025-12-07 09:05:54 +0200

    Add SaveItemCommand

---
+++ KooliProjekt.Application/Features/Items/SaveItemCommand.cs
@@ -0,0 +1,18 @@
+﻿using KooliProjekt.Application.Behaviors;
+using KooliProjekt.Application.Data;
+using KooliProjekt.Application.Infrastructure.Results;
+using MediatR;
+
+namespace KooliProjekt.Application.Features.Items
+{
+    public class SaveItemCommand : IRequest<OperationResult>, ITransactional
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+        public Invoice Invoice { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
