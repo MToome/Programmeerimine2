8f1fef0623eb29519a2cbd3f27025d96d44eb322
Marek Toome <marekumail@gmail.com>
2025-12-13 10:03:39 +0200

    Add custom validation rule example in SaveCustomerCommandValidator

--- KooliProjekt.Application/Features/Customers/SaveCustomerCommandValidator.cs
+++ KooliProjekt.Application/Features/Customers/SaveCustomerCommandValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using KooliProjekt.Application.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,27 @@ namespace KooliProjekt.Application.Features.Customers
 {
     public class SaveCustomerCommandValidator : AbstractValidator<SaveCustomerCommand>
     {
-        public SaveCustomerCommandValidator()
+        public SaveCustomerCommandValidator(ApplicationDbContext context)
         {
             RuleFor(c => c.Name)
                 .NotEmpty().WithMessage("Customer name is required.")
-                .MaximumLength(100).WithMessage("Customer name must not exceed 100 characters.");
+                .MaximumLength(100).WithMessage("Customer name must not exceed 100 characters.")
+                // Oma loogikaga valideerimise reegel
+                // Siin võib kasutada DbContexti klassi
+                .Custom((s, context) =>
+                 {
+                     // Command või query, mida valideerime
+                     var command = context.InstanceToValidate;
+
+                     // Oma valideerimise loogika
+                     // koos vea lisamisega
+                     //var failure = new ValidationFailure();
+                     //failure.AttemptedValue = command.ProjectId;
+                     //failure.ErrorMessage = "Cannot find project with id " + command.ProjectId;
+                     //failure.PropertyName = nameof(command.ProjectId);
+
+                     //context.AddFailure(failure);
+                 });
             RuleFor(c => c.Email)
                 .NotEmpty().WithMessage("Email is required.")
                 .EmailAddress().WithMessage("A valid email is required.");
@@ -24,6 +41,7 @@ namespace KooliProjekt.Application.Features.Customers
                 .NotEmpty().WithMessage("Address is required.");
             RuleFor(c => c.City)
                 .NotEmpty().WithMessage("City is required.");
+            
         }
     }
 }
