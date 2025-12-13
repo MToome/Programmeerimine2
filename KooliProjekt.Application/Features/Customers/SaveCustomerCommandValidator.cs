0d46460d8c2f4cbbf996c72102c13e4b3c8f2026
Marek Toome <marekumail@gmail.com>
2025-12-13 09:51:48 +0200

    Add SaveCustomerCommandValidator

---
+++ KooliProjekt.Application/Features/Customers/SaveCustomerCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KooliProjekt.Application.Features.Customers
+{
+    public class SaveCustomerCommandValidator : AbstractValidator<SaveCustomerCommand>
+    {
+        public SaveCustomerCommandValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Customer name is required.")
+                .MaximumLength(100).WithMessage("Customer name must not exceed 100 characters.");
+            RuleFor(c => c.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("A valid email is required.");
+            RuleFor(c => c.Phone)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .MaximumLength(15).WithMessage("Phone number must not exceed 15 characters.");
+            RuleFor(c => c.Address)
+                .NotEmpty().WithMessage("Address is required.");
+            RuleFor(c => c.City)
+                .NotEmpty().WithMessage("City is required.");
+        }
+    }
+}
