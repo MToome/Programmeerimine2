fb9027a37058d31327079d5bfea7d6bd4effae35
Marek Toome <marekumail@gmail.com>
2026-03-15 08:17:00 +0200

    Add Save and Delete customer to windows forms

--- KooliProjekt.WindowsForms/Form1.Designer.cs
+++ KooliProjekt.WindowsForms/Form1.Designer.cs
@@ -28,24 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
+            CustomerGridView1 = new DataGridView();
             NameBox = new TextBox();
             AddressBox = new TextBox();
             CityBox = new TextBox();
             PhoneBox = new TextBox();
             EmailBox = new TextBox();
-            DiscountBox = new TextBox();
             SubmitBtn = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            DicountUpDown = new NumericUpDown();
+            DeleteBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)CustomerGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DicountUpDown).BeginInit();
             SuspendLayout();
             // 
-            // dataGridView1
+            // CustomerGridView1
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 12);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(776, 150);
-            dataGridView1.TabIndex = 0;
+            CustomerGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            CustomerGridView1.Dock = DockStyle.Top;
+            CustomerGridView1.Location = new Point(0, 0);
+            CustomerGridView1.Name = "CustomerGridView1";
+            CustomerGridView1.Size = new Size(800, 150);
+            CustomerGridView1.TabIndex = 0;
+            CustomerGridView1.CellContentClick += CustomerGridView1_CellContentClick;
             // 
             // NameBox
             // 
@@ -92,19 +96,9 @@
             EmailBox.TabIndex = 5;
             EmailBox.TextChanged += EmailBox_TextChanged;
             // 
-            // DiscountBox
-            // 
-            DiscountBox.Location = new Point(12, 364);
-            DiscountBox.Name = "DiscountBox";
-            DiscountBox.PlaceholderText = "Enter discount %";
-            DiscountBox.Size = new Size(201, 23);
-            DiscountBox.TabIndex = 6;
-            DiscountBox.WordWrap = false;
-            DiscountBox.TextChanged += DiscountBox_TextChanged;
-            // 
             // SubmitBtn
             // 
-            SubmitBtn.Location = new Point(68, 393);
+            SubmitBtn.Location = new Point(38, 393);
             SubmitBtn.Name = "SubmitBtn";
             SubmitBtn.Size = new Size(75, 23);
             SubmitBtn.TabIndex = 7;
@@ -112,36 +106,60 @@
             SubmitBtn.UseVisualStyleBackColor = true;
             SubmitBtn.Click += SubmitBtn_Click;
             // 
+            // DicountUpDown
+            // 
+            DicountUpDown.DecimalPlaces = 1;
+            DicountUpDown.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            DicountUpDown.Location = new Point(38, 364);
+            DicountUpDown.Maximum = new decimal(new int[] { 1, 0, 0, 0 });
+            DicountUpDown.Name = "DicountUpDown";
+            DicountUpDown.Size = new Size(120, 23);
+            DicountUpDown.TabIndex = 8;
+            DicountUpDown.ValueChanged += DicountUpDown_ValueChanged;
+            // 
+            // DeleteBtn
+            // 
+            DeleteBtn.Location = new Point(138, 393);
+            DeleteBtn.Name = "DeleteBtn";
+            DeleteBtn.Size = new Size(75, 23);
+            DeleteBtn.TabIndex = 9;
+            DeleteBtn.Text = "Delete";
+            DeleteBtn.UseVisualStyleBackColor = true;
+            DeleteBtn.Click += DeleteBtn_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(DeleteBtn);
+            Controls.Add(DicountUpDown);
             Controls.Add(SubmitBtn);
-            Controls.Add(DiscountBox);
             Controls.Add(EmailBox);
             Controls.Add(PhoneBox);
             Controls.Add(CityBox);
             Controls.Add(AddressBox);
             Controls.Add(NameBox);
-            Controls.Add(dataGridView1);
+            Controls.Add(CustomerGridView1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             Name = "Form1";
             Text = "Eshop";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)CustomerGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DicountUpDown).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private DataGridView CustomerGridView1;
         private TextBox NameBox;
         private TextBox AddressBox;
         private TextBox CityBox;
         private TextBox PhoneBox;
         private TextBox EmailBox;
-        private TextBox DiscountBox;
         private Button SubmitBtn;
+        private NumericUpDown DicountUpDown;
+        private Button DeleteBtn;
     }
 }
--- KooliProjekt.WindowsForms/Form1.cs
+++ KooliProjekt.WindowsForms/Form1.cs
@@ -1,10 +1,19 @@
+using KooliProjekt.Application.Features.Customers;
 using KooliProjekt.WindowsForms.Api;
 using System.Net.Http.Json;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace KooliProjekt.WindowsForms
 {
     public partial class Form1 : Form
     {
+
+        private readonly HttpClient _httpClient = new HttpClient 
+        { 
+            BaseAddress = new Uri("http://localhost:5086/")
+
+        };
+
         public Form1()
         {
             InitializeComponent();
@@ -20,14 +29,14 @@ namespace KooliProjekt.WindowsForms
         private async Task LoadData()
         {
             var url = "http://localhost:5086/api/Customer/List";
-            url += "?page=1&pageSize=10";
+            url += "?page=1&pageSize=25";
 
             using var client = new HttpClient(); // Using statement ensures proper disposal of HttpClient
 
             var response = await client.GetFromJsonAsync<OperationResult<PagedResult<Customer>>>(url);
             // response: OperationResult<PagedResult<Customer>>
 
-            dataGridView1.DataSource = response.Value.Results;
+            CustomerGridView1.DataSource = response.Value.Results;
         }
 
         private void NameBox_TextChanged(object sender, EventArgs e)
@@ -55,12 +64,75 @@ namespace KooliProjekt.WindowsForms
 
         }
 
-        private void DiscountBox_TextChanged(object sender, EventArgs e)
+        private void DicountUpDown_ValueChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void SubmitBtn_Click(object sender, EventArgs e)
+        private async void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            var url = "http://localhost:5086/api/customer/Save";
+
+            var command = new SaveCustomerCommand
+            {
+                Name = NameBox.Text,
+                Address = AddressBox.Text,
+                Phone = PhoneBox.Text,
+                Email = EmailBox.Text,
+                City = CityBox.Text,
+                Discount = DicountUpDown.Value,
+            };
+
+            var response = await _httpClient.PostAsJsonAsync(url, command);
+
+            if (response.IsSuccessStatusCode)
+                MessageBox.Show("Customer saved!");
+            else
+                MessageBox.Show("Error saving customer");
+
+            await LoadData();
+        }
+
+        private async void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            if (CustomerGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a customer to delete.");
+                return;
+            }
+
+            var confirmResult = MessageBox.Show(
+                "Are you sure you want to delete this customer?",
+                "Confirm Delete",
+                MessageBoxButtons.YesNo);
+
+            if (confirmResult != DialogResult.Yes)
+                return;
+
+            var url = "api/customer/delete"; // Adjust the URL as needed
+
+            int id = (int)CustomerGridView1.SelectedRows[0].Cells["Id"].Value;
+
+            var command = new DeleteCustomerCommand { Id = id };
+            var request = new HttpRequestMessage(HttpMethod.Delete, url)
+            {
+                Content = JsonContent.Create(command)
+            };
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Customer deleted!");
+                await LoadData(); // Refresh the data grid after deletion
+            }
+            else
+            {
+                MessageBox.Show("Delete failed.");
+            }
+        }
+
+        private void CustomerGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
